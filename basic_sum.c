63a9478f10dd291498c6c004eb751e150bd2169b
Tanmay Yadav <145816497+Tanmayyp01@users.noreply.github.com>
2023-10-12 07:05:55 +0530

    Add files via upload
    Basic Sum code added

---
+++ basic_sum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+
+    int a , b , c ; // Varaiable Declaerd.
+
+    // Taking Inputs :
+    printf("Please Enter 1st Integer : ");
+    scanf("%d", &a);
+
+    printf("Please Enter 2nd Integer :");
+    scanf("%d", &b);
+
+    // Sum :
+    c = a + b;
+
+    // Printing Output of Sum :
+
+    printf("Sum of Two Given Integer is : %d", c);
+
+    return 0;
+}
